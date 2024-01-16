2cf2577b0e5cebf728a555218425b0f6b39c7ce2
unknown <ainbalqis0305@gmail.com>
2024-01-16 22:14:37 +0800

    AVOID UNUSED IMPORT

--- src/java/BookDetailsController.java
+++ src/java/BookDetailsController.java
@@ -1,6 +1,8 @@
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import store.Book;
 import store.Store;
 
